860e0d7f26d05823aabdab2da91011ec7557382c
b-prasanth9 <r02737758@gmail.com>
2026-09-14 10:04:52 +0530

    Add VitalGuard application

---
+++ src/main/java/com/prasanth/vitalguard/VitalguardApplication.java
@@ -0,0 +1,3 @@
+package com.prasanth.vitalguard;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class VitalguardApplication{public static void main(String[] args){SpringApplication.run(VitalguardApplication.class,args);}}
